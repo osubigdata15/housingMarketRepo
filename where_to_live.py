93c51b1d9f06cd13203bbae54aea697a6b7c6859
osubigdata15 <Morosky.4@osu.edu>
2015-11-11 17:46:12 -0500

    Update where_to_live.py

--- where_to_live.py
+++ where_to_live.py
@@ -6,13 +6,15 @@ import numpy
 parser = argparse.ArgumentParser(description = "Bang for your Buck Location Calculator")
 parser.add_argument("median_income_file", type = str, help = "Csv file containing median wage information")
 parser.add_argument("zillow_data", type = str, help = "Csv file containing zhvi indexes for all states")
+parser.add_argument("occupation", type = str, help = "Occupation code")
+
 args = parser.parse_args()
 
 state_code_path = "state_codes.csv"
-occupation = "5380"
-def toInt(x):
+occupation = args.occupation
+def toFloat(x):
     try:
-        return int(x)
+        return float(x)
     except ValueError:
         return 0
 
@@ -26,6 +28,9 @@ def createZillowDict(filename):
 	d[housing_array[i][1]] = housing_array[i][3]
     return d
 
+with open(state_code_path, 'Ur') as f:
+    reader = csv.reader(f, delimiter=",")
+    stateDict = dict(reader)
 
 with open(args.median_income_file) as f:
     reader = csv.reader(f, delimiter=",")
@@ -34,30 +39,55 @@ with open(args.median_income_file) as f:
     wageArray = list(reader)
     wageDict = {}
     for i in range(1, len(wageArray)):
-        state = wageArray[i][0]
-        wages = []
+        code = wageArray[i][0]
+        wagesPerState = {}
         for j in range(1, len(wageArray[i])):
-            wages.append(wageArray[i][j])
-        wageDict[state]=wages
+            
+            state = wageArray[0][j]
+            wagesPerState[state]=(wageArray[i][j])
+        wageDict[code]=wagesPerState
 
-with open(state_code_path) as f:
-    reader = csv.reader(f, delimiter=",")
 
-    #column 0 is occupation code. all other columns are median wage per state.
-    stateDict = dict(reader)
 
 zillowDict = createZillowDict(args.zillow_data)
-BBDict = {}
+BBDict = collections.OrderedDict()
+
+zeroStates = ['20', '23','56']
+
+#print toInt(wageDict[occupation]['54'])
+#print toInt(zillowDict[stateDict['54']])
 
 #want Dictionary of State: wage-homecost
-
-i=0
 for state in stateDict:
-    BBDict[stateDict[state]]=toInt(wageDict[occupation][i])-toInt(zillowDict[stateDict[state]])
-    i+=1
+    
+    if state not in zeroStates:
+        BBDict[stateDict[state]]= toFloat(wageDict[occupation][state])- toFloat(zillowDict[stateDict[state]])
 
-print BBDict
+#print BBDict
 
 #maine, kansas, and wyoming have no zillow data
+
+
+BBDict = collections.OrderedDict(sorted(BBDict.iteritems(), key=lambda x: x[1], reverse = True))
+i = 0
+print "The top five places for you to live are: "
+for entry in BBDict:
+    if(i < 5):
+        print entry
+        i+=1
+    else:
+        break
+
+fileOutName = "BFYB_"+str(occupation)+".csv"
+with open(fileOutName, 'w') as f:
+    output = []
+    for entry in BBDict:
+        output.append(entry)
+    print >> f, ",".join(output)
+    output = []
+    for entry in BBDict:
+        output.append(str(BBDict[entry]))
+    print >> f, ",".join(output)
+
 
 
